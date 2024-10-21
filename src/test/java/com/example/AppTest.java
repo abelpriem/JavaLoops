32d8c30a35777ab0a56cb6aa6dcf689af1a2489d
abelpriem <abelpriem94@hotmail.com>
2024-10-21 09:46:18 +0200

    inicializar repositorio remoto

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
